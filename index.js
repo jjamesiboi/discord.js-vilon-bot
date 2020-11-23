9a737110710997f7c22a4ae24245f600cdce79d6
James Messer <71551059+CodesWithJames@users.noreply.github.com>
2020-11-23 20:53:13 +0100

    lokl

--- index.js
+++ index.js
@@ -31,6 +31,15 @@ client.on("ready", () => {
   }
   setInterval(status, 3)
 });
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+});
+
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+});
 
 //IS URL FUNCTION - START
 
