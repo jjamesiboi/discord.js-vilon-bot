5b3c18daefd151b33e0e6b586a4421d5126438e7
James Messer <71551059+CodesWithJames@users.noreply.github.com>
2020-11-24 16:15:51 +0100

    sasasdasdada

---
+++ commands/leveling/level.js
@@ -0,0 +1,52 @@
+const { MessageEmbed } = require("discord.js");
+const db = require('quick.db');
+const { default_prefix } = require('../../config');
+
+m
+module.exports = {
+  name: "level",
+  usage: "level",
+  description: "Shows ur level.",
+  category: "leveling",
+  run: (client, message, args) => {
+   
+        try {
+            let prefix;
+            let fetched = await db.fetch(`prefix_${message.guild.id}`)
+            if (fetched === null) {
+                prefix = default_prefix
+            } else {
+                prefix = fetched
+            }
+            let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
+            if (!user) return message.channel.send('**Please Enter A Valid User!**')
+            if (user.user.bot) return message.channel.send(`**Bot's Dont Have XP Level, LOL**`);
+
+            let guildMessages = db.fetch(`guildMessages_${message.guild.id}`)
+            if (guildMessages === null) return message.channel.send(`**Levelling Messages Are Currently Disabled!\n\`${default_prefix}setxp\` To Enable**`);
+
+            let xp = db.fetch(`messages_${message.guild.id}_${user.id}`)
+            let lvl = db.fetch(`level_${message.guild.id}_${user.id}`)
+
+            if (lvl === null) lvl = 0
+            if (xp === null) xp = 0
+
+            let curxp = xp;
+            let curlvl = lvl;
+            let nxtLvlXp = curlvl * 100;
+            let difference2 = nxtLvlXp + 100 - curxp;
+
+            const embed = new MessageEmbed()
+                .setTitle(`**${user.displayName}**`)
+                .setColor("00FFEC")
+                .setDescription(`**Level - \`${curlvl}\` | XP - \`${curxp - 1}\`
+                    Needed XP To Reach Next Level - \`${difference2 + 1}\`**
+                    `)
+                .setFooter(message.guild.name, message.guild.iconURL())
+            message.channel.send(embed)
+
+        } catch {
+            message.channel.send(`**Oh No! An Error Occurred!**`);
+        }
+    }
+};
--- index.js
+++ index.js
@@ -116,44 +116,158 @@ client.on("message", async message => {
 }); //All codes link in description
 
 //GONNA USE EVENT HERE
+client.on('guildMemberAdd', async member => {
 
-client.on("guildMemberAdd", async member => {
-  let chx = db.get(`welchannel_${member.guild.id}`);
+  let wChan = db.fetch(`welcome_${member.guild.id}`)
 
-  if (chx === null) {
-    return;
-  }
+  if (wChan == null) return;
 
+  if (!wChan) return;
 
-  let data = await canva.welcome(member, { link: "https://i.pinimg.com/originals/f3/1c/39/f31c39d56512dc8fbf30f9d0fb3ee9d3.jpg" })
+  let font64 = await jimp.loadFont(jimp.FONT_SANS_64_WHITE)
+  let bfont64 = await jimp.loadFont(jimp.FONT_SANS_64_WHITE)
+  let mask = await jimp.read('https://i.imgur.com/552kzaW.png')
+  let welcome = await jimp.read('https://cdn.discordapp.com/attachments/764408078363525143/780647928364007424/Desktop_Wallpaper_Space-711121.jpg')
 
-  const attachment = new discord.MessageAttachment(
-    data,
-    "welcome-image.png"
-  );
+  jimp.read(member.user.displayAvatarURL({ format: 'png' })).then(avatar => {
+      avatar.resize(200, 200)
+      mask.resize(200, 200)
+      avatar.mask(mask)
+      welcome.resize(1000, 300)
 
+      welcome.print(font64, 265, 55, `Welcome ${member.user.username}`)
+      welcome.print(bfont64, 265, 125, `To ${member.guild.name}`)
+      welcome.print(font64, 260, 190, `Member #${member.guild.memberCount}`)
+      welcome.composite(avatar, 40, 55).write('Welcome2.png')
+      try {
+          member.guild.channels.cache.get(wChan).send(``, { files: ["Welcome2.png"] })
+      } catch (e) {
+        
+      }
+  })
+      var r = member.guild.roles.cache.find(r => r.name === 'Community');
+      if (!r) return;
+      member.roles.add(r)
 
+});
+client.on('guildMemberRemove', async member => {
 
+  let wChan = db.fetch(`welcome_${member.guild.id}`)
 
-  client.channels.cache.get(chx).send("Welcome to our Server " + member.user.username, attachment);
+  if (wChan == null) return;
+
+  if (!wChan) return;
+
+  let font64 = await jimp.loadFont(jimp.FONT_SANS_64_WHITE)
+  let bfont64 = await jimp.loadFont(jimp.FONT_SANS_64_WHITE)
+  let mask = await jimp.read('https://i.imgur.com/552kzaW.png')
+  let welcome = await jimp.read('https://cdn.discordapp.com/attachments/764408078363525143/780647928364007424/Desktop_Wallpaper_Space-711121.jpg')
+
+  jimp.read(member.user.displayAvatarURL({ format: 'png' })).then(avatar => {
+      avatar.resize(200, 200)
+      mask.resize(200, 200)
+      avatar.mask(mask)
+      welcome.resize(1000, 300)
+
+      welcome.print(font64, 265, 55, `Bye Bye ${member.user.username}`)
+      welcome.print(bfont64, 265, 125, `He/She left ${member.guild.name}`)
+      welcome.print(font64, 260, 190, `You now have ${member.guild.memberCount} members `)
+      welcome.composite(avatar, 40, 55).write('Welcome2.png')
+      try {
+          member.guild.channels.cache.get(wChan).send(``, { files: ["Welcome2.png"] })
+      } catch (e) {
+        
+      }
+  })
 });
 
-client.on('message', (message) => {
-  if (message.content) {
-    const profane = !!profanity.find((word) => {
-      const regex = new RegExp(`\\b${word}\\b`, 'i'); // if the phrase is not alphanumerical,
-      return regex.test(message.content);             // you may need to escape tokens
-    });
 
-    if (profane) {
-      message.channel.send(`<@${message.author.id}> dont use swear words!`)
-      return message.delete()
-        .catch(console.error);
-    }
-  }
+client.on('message', async message => {
+  let prefix;
+  if (message.author.bot || message.channel.type === "dm") return;
+      try {
+          let fetched = await db.fetch(`prefix_${message.guild.id}`);
+          if (fetched == null) {
+              prefix = PREFIX
+          } else {
+              prefix = fetched
+          }
+      } catch (e) {
+          console.log(e)
+  };
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let messageFetch = db.fetch(`guildMessages_${message.guild.id}`)
+  if (messageFetch === null) return;
+
+  db.add(`messages_${message.guild.id}_${message.author.id}`, 1)
+  let messagefetch = db.fetch(`messages_${message.guild.id}_${message.author.id}`)
+
+  let messages;
+  if (messagefetch == 0) messages = 0; //Level 0
+  else if (messagefetch == 100) messages = 100; // Level 1
+  else if (messagefetch == 200) messages = 200; // Level 2
+  else if (messagefetch == 300) messages = 300; // Level 3
+  else if (messagefetch == 400) messages = 400; // Level 4
+  else if (messagefetch == 500) messages = 500; // Level 5
+  else if (messagefetch == 600) messages = 600; // Level 6
+  else if (messagefetch == 700) messages = 700; // Level 7
+  else if (messagefetch == 800) messages = 800; // Level 8
+  else if (messagefetch == 900) messages = 900; // Level 9
+  else if (messagefetch == 1000) messages = 1000; // Level 10
+  else if (messagefetch == 1100) messages = 1100; // Level 11
+  else if (messagefetch == 1200) messages = 1200; // Level 12
+  else if (messagefetch == 1300) messages = 1300; // Level 13
+  else if (messagefetch == 1400) messages = 1400; // Level 14
+  else if (messagefetch == 1500) messages = 1500; // Level 15
+  else if (messagefetch == 1600) messages = 1600; // Level 16
+  else if (messagefetch == 1700) messages = 1700; // Level 17
+  else if (messagefetch == 1800) messages = 1800; // Level 18
+  else if (messagefetch == 1900) messages = 1900; // Level 19
+  else if (messagefetch == 2000) messages = 2000; // Level 20
+  else if (messagefetch == 2100) messages = 2100; // Level 21
+  else if (messagefetch == 2200) messages = 2200; // Level 22
+  else if (messagefetch == 2300) messages = 2300; // Level 23
+  else if (messagefetch == 2400) messages = 2400; // Level 24
+  else if (messagefetch == 2500) messages = 2500; // Level 25
+  else if (messagefetch == 2600) messages = 2600; // Level 26
+  else if (messagefetch == 2700) messages = 2700; // Level 27
+  else if (messagefetch == 2800) messages = 2800; // Level 28
+  else if (messagefetch == 2900) messages = 2900; // Level 29
+  else if (messagefetch == 3000) messages = 3000; // Level 30
+  else if (messagefetch == 3100) messages = 3100; // Level 31
+  else if (messagefetch == 3200) messages = 3200; // Level 32
+  else if (messagefetch == 3300) messages = 3300; // Level 33
+  else if (messagefetch == 3400) messages = 3400; // Level 34
+  else if (messagefetch == 3500) messages = 3500; // Level 35
+  else if (messagefetch == 3600) messages = 3600; // Level 36
+  else if (messagefetch == 3700) messages = 3700; // Level 37
+  else if (messagefetch == 3800) messages = 3800; // Level 38
+  else if (messagefetch == 3900) messages = 3900; // Level 39
+  else if (messagefetch == 4000) messages = 4000; // Level 40
+  else if (messagefetch == 4100) messages = 4100; // Level 41
+  else if (messagefetch == 4200) messages = 4200; // Level 42
+  else if (messagefetch == 4300) messages = 4300; // Level 43
+  else if (messagefetch == 4400) messages = 4400; // Level 44
+  else if (messagefetch == 4500) messages = 4500; // Level 45
+  else if (messagefetch == 4600) messages = 4600; // Level 46
+  else if (messagefetch == 4700) messages = 4700; // Level 47
+  else if (messagefetch == 4800) messages = 4800; // Level 48
+  else if (messagefetch == 4900) messages = 4900; // Level 49
+  else if (messagefetch == 5000) messages = 5000; // level 50
+
+  if (!isNaN(messages)) {
+      db.add(`level_${message.guild.id}_${message.author.id}`, 1)
+      let levelfetch = db.fetch(`level_${message.guild.id}_${message.author.id}`)
+
+      let levelembed = new MessageEmbed()
+          .setColor('00FFEC')
+          .setDescription(`**${message.author}, You Have Leveled Up To Level ${levelfetch}**`)
+          .setFooter(`&disablexp To Disable Level Up Messages`)
+      message.channel.send(levelembed);
+  };
 });
-
-
-
 // Login the bot
 client.login(process.env.TOKEN);
