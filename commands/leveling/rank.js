20f20df41d3d4b1c1177bc99faa373b2536bc8e8
James Messer <71551059+CodesWithJames@users.noreply.github.com>
2020-11-25 11:17:49 +0100

    adwwwww

---
+++ commands/leveling/rank.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js')
+const Levels = require('discord-xp');
+const canvacord = require("canvacord")
+module.exports = {
+    name: "rank",
+    category: "leveling",
+    description: "Get your level and xp",
+    usage: "rank",
+    run: (client, message) => {
+     
+      const target = message.mentions.users.first() || message.author; // Grab the target.
+ 
+      const user = await Levels.fetch(target.id, message.guild.id); // Selects the target from the database.
+       
+      if (!user) return message.channel.send("You dont have xp, send some messages to earn some!"); // If there isnt such user in the database, we send a message in general.
+
+      const rank = new canvacord.Rank()
+          .setAvatar(message.athor.DislayAvatarURL({ dynamic: false, format: 'png' }))
+          .setCurrentXP(user.xp)
+          .setRequiredXP(neededXp)
+          .setStatus(message.member.presence.status)
+          .setProgressBar("#FFA500", "COLOR")
+          .setUsername(message.author.username)
+          .setDiscriminator("0001");
+       
+      rank.build()
+          .then(data => {
+              const attachment = new Discord.MessageAttachment(data, "RankCard.png");
+              message.channel.send(attachment);
+            });
+          }
+        }
--- index.js
+++ index.js
@@ -102,7 +102,17 @@ client.on("message", async message => {
 
 //GONNA USE EVENT HERE
 
-
+client.on("message", async (message) => {
+  if (!message.guild) return;
+  if (message.author.bot) return;
+  
+  const randomAmountOfXp = Math.floor(Math.random() * 29) + 1; // Min 1, Max 30
+  const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp);
+  if (hasLeveledUp) {
+    const user = await Levels.fetch(message.author.id, message.guild.id);
+    message.channel.send(`${message.author}, congratulations! You have leveled up to **${user.level}**. :tada:`);
+  }
+});
 
 // Login the bot
 client.login(process.env.TOKEN);
