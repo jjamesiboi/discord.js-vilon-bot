81ca8d4667130cf3f9e1fb9edfc5c679353a3113
James Messer <71551059+CodesWithJames@users.noreply.github.com>
2020-11-25 11:35:47 +0100

    afwfawfawf

--- commands/leveling/rank.js
+++ commands/leveling/rank.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js')
-const canvacord = require("canvacord")
+const Levels = require('discord-xp');
 module.exports = {
     name: "rank",
     category: "leveling",
